e3e8bde0edcb35ea476e6785ca4c3bc6d3b6d554
yuzalle <yuzalwahyudi2001@gmail.com>
2024-05-23 17:27:43 +0700

    commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+let isKelilingVisible = false;
+function hideOrshow() {
+    if (isKelilingVisible == false){
+        isKelilingVisible = true;
+        document.getElementById('luas-section').style.display='none';
+        document.getElementById('keliling-section').style.display='block';
+    } else{
+        isKelilingVisible = false;
+        document.getElementById('luas-section').style.display='block';
+        document.getElementById('keliling-section').style.display='hide';
+    }
+    
+}
+
+function luassegitiga(){
+    let alasValue = document.getElementById('alas-input').value;
+    let tinggiValue = document.getElementById('tinggi-input').value;
+    let luas = 0.5*alasValue*tinggiValue;
+
+    console.log(alasValue);
+    console.log(tinggiValue);
+    console.log(luas);
+
+    if (alasValue != '' && tinggiValue != ''){
+        console.log('sukses validasi')
+    } else{
+        alert('kosong')
+    }
+    document.getElementById('luas-input').value=luas
+}
+
+function kelilingsegitiga(){
+    let s1Value = parseFloat(document.getElementById('S1-input').value);
+    let s2Value = parseFloat(document.getElementById('S2-input').value);
+    let s3Value = parseFloat(document.getElementById('S3-input').value);
+    let keliling = s1Value+s2Value+s3Value;
+
+    console.log(s1Value);
+    console.log(s2Value);
+    console.log(s3Value);
+    console.log(keliling);
+
+    if (s1Value != '' && s2Value != '' && s3Value != ''){
+        console.log('sukses validasi')
+    } else{
+        alert('kosong')
+    }
+    document.getElementById('keliling-input').value=keliling
+}
